69614e86db01cf9622c9bc4767949596d3fa8253
Aleksa Drozdova <das8769@gmail.com>
2019-03-25 17:13:21 +1000

    готовый сайт

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@ var close = popup.querySelector(".modal-close");
 
 var link2 = document.querySelector(".price-color");
 var popup2 = document.querySelector(".color");
-var close2 = popup.querySelector(".modal-close");
+var close2 = popup2.querySelector(".modal-close");
 
 var link3 = document.querySelector(".price-treatment");
 var popup3 = document.querySelector(".treatment");
-var close3 = popup.querySelector(".modal-close");
+var close3 = popup3.querySelector(".modal-close");
+
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
@@ -37,11 +38,28 @@ close3.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup3.classList.remove("modal-show");
 });
+
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
         evt.preventDefault();
         if (popup.classList.contains("modal-show")) {
             popup.classList.remove("modal-show");
+        }
+    }
+});
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup2.classList.contains("modal-show")) {
+            popup2.classList.remove("modal-show");
+        }
+    }
+});
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup3.classList.contains("modal-show")) {
+            popup3.classList.remove("modal-show");
         }
     }
 });
